a4373d6bec09c3d2c1a2602623eef0b0e5f0a531
Sandip Rode <16761010+Sandip1447@users.noreply.github.com>
2022-11-10 09:17:20 +0530

    State in compose

--- app/src/main/java/com/freecodecloud/composetutorial/presentation/screen/StateInCompose.kt
+++ app/src/main/java/com/freecodecloud/composetutorial/presentation/screen/StateInCompose.kt
@@ -1,3 +1,67 @@
 package com.freecodecloud.composetutorial.presentation.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.freecodecloud.composetutorial.presentation.ui.theme.ComposeTutorialTheme
+
 //https://developer.android.com/codelabs/jetpack-compose-state#0
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var counter by rememberSaveable { mutableStateOf(0) }
+
+    StatelessCounter(modifier = modifier, counter = counter, onIncrement = { counter++ })
+
+}
+
+@Composable
+private fun StatelessCounter(
+    modifier: Modifier,
+    counter: Int,
+    onIncrement: () -> Unit
+) {
+    Column(modifier = modifier.padding(16.dp)) {
+
+        if (counter > 0) {
+            Text(text = "You've had $counter glasses.")
+        }
+        Button(
+            onClick = onIncrement,
+            enabled = counter < 10,
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text(text = "Add one")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WaterCounterPreview() {
+    ComposeTutorialTheme {
+        StatefulCounter()
+    }
+}
+
+
+@Composable
+fun EmailField(modifier: Modifier = Modifier) {
+    TextField(value = "", onValueChange = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EmailFieldPreview() {
+
+}
